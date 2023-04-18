87159a093091aaaabfc1a6f98756c9fbb6b1aad1
pedro-iesb <pedro.santos3@iesb.edu.br>
2023-04-17 22:19:32 -0300

    api filmes

--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -1,14 +1,29 @@
 import Pagina from '@/components/Pagina'
 import apiDeputados from '@/services/apiDeputados'
 import apiFilmes from '@/services/apiFilmes'
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Row } from 'react-bootstrap'
 
 const index = (props) => {
     return (
         <Pagina titulo="Filmes">
-            {props.filmes.map(item => (
-                <p>{item.title}</p>
-            ))}
+
+            <Row md={4}>
+                {props.filmes.map(item => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + item.backdrop_path} />
+                            <Card.Body>
+                                <Card.Title>{item.title}</Card.Title>
+                               <p>lançamento: {item.release_date}</p>
+                               <p>nota: {item.vote_average}</p>
+                               <Link className='btn btn-danger' href={'/filmes/' + item.id} >detalhes</Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </Pagina>
     )
 }
